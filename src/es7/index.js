b3a58c8d47805356ff4fa48099180e970666f6cc
oscarfiscal <oscarfiscalcp@gmail.com>
2022-03-22 18:34:11 -0500

    es7

---
+++ src/es7/index.js
@@ -0,0 +1,15 @@
+let numbers =[1,2,3,4,5,6,7,8,9,10];
+
+if (numbers.includes(12)){
+    console.log('si se encuentra el numero 7');
+}else{
+    console.log('no  se encuentra el numero 7');
+}   // includes() encontrar un elemento en un array
+
+//elevar a la potencia
+
+let base = 4;
+let exponent = 3;
+let result =base ** exponent;
+
+console.log(result);
